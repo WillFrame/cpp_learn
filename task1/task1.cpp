0a71db4c70f2ef273b6a2097c2e27af4ac65149d
WillFrame <mozhevm@gmail.com>
2017-10-26 17:20:47 +0300

    сравнение с единицей

--- task1/task1.cpp
+++ task1/task1.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main() {
     int x, y;
     cin >> x >> y;
+    if (x == 1) {
+        cout << "x is 1" << endl;
+    }
+
     cout << x << " + " << y << " = " << x + y << endl << x << " * " << y << " = " << x * y;
     return 0;
 }
