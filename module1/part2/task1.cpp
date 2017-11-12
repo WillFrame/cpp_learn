eedbc219de3711148f20f0b914fcfcddfff14dc1
WillFrame <32850788+WillFrame@users.noreply.github.com>
2017-11-12 21:51:45 +0300

    module1
    module1

---
+++ module1/part2/task1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+    cin >> x;
+    if (x >= y) { cout << x; }
+    else {
+        cout << y;
+    }
+    return 0;
+}
---
+++ module1/part2/task4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    if (x > y) {
+        if (x > z) {
+            if (x < y + z) {
+                cout << "Yes";
+            } else { cout << "No"; }
+        }
+    } else {
+        if (y > z) {
+            if (y < x + z) {
+                cout << "Yes";
+            } else { cout << "No"; }
+        } else {
+            if (z < x + y) {
+                cout << "Yes";
+            } else { cout << "No"; }
+        }
+    }
+    return 0;
+}
