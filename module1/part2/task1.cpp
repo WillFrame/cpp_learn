1b53c886c9dce99e3b5d48a085cc5a80f77b0c61
WillFrame <mozhevm@gmail.com>
2017-11-12 22:11:02 +0300

    Refactor files 1,2

--- module1/part2/task1.cpp
+++ module1/part2/task1.cpp
@@ -5,9 +5,12 @@ using namespace std;
 int main() {
     int x, y;
     cin >> x;
-    if (x >= y) { cout << x; }
-    else {
+
+    if (x >= y) {
+        cout << x;
+    } else {
         cout << y;
     }
+
     return 0;
 }
--- module1/part2/task2.cpp
+++ module1/part2/task2.cpp
@@ -5,10 +5,16 @@ using namespace std;
 int main() {
     int x, y;
     cin >> x >> y;
-    if (x > y) { cout << 1; }
-    else {
-        if (x < y) { cout << 2; }
-        else { cout << 0; }
+
+    if (x > y) {
+        cout << 1;
+    } else {
+        if (x < y) {
+            cout << 2;
+        } else {
+            cout << 0;
+        }
     }
+    
     return 0;
 }
