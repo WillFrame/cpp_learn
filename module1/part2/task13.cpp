e58003507ed272489de808f5d6d1a4a6bde9a2c0
WillFrame <mozhevm@gmail.com>
2017-11-18 23:44:39 +0300

    not done

---
+++ module1/part2/task13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+   int f1 = 1, f2 = 1, fw = 0, a, b = 2;
+    cin >> a;
+    while (a > fw) {
+        fw = f1 + f2;
+        f1 = f2;
+        f2 = fw;
+        ++b;
+    }
+    fw = fw - f1;
+    if (a == fw) {
+        cout << b;
+    } else {
+        cout << "-1";
+    }
+    return 0;
+}
