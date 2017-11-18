bb6bc6481a4383d55c05f16206b08d8b4713b7f9
WillFrame <mozhevm@gmail.com>
2017-11-18 14:52:08 +0300

    done

---
+++ module1/part2/task9.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x, y;
+
+    cin >> x;
+    y = 1;
+    while (x > y) {
+        cout << y << " ";
+        y = y * 2;
+    }
+
+    return 0;
+}
