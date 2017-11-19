605c1fda5c8b31e5f09dc5ff07b27d7d41df69f0
WillFrame <mozhevm@gmail.com>
2017-11-19 23:17:11 +0300

    fixed

--- module1/part3/task4.cpp
+++ module1/part3/task4.cpp
@@ -7,12 +7,14 @@ int main () {
     int x,y,z;
     double w;
 
+    z = 0;
+    y = 0;
     while (x != 0) {
         cin >> x;
         z += x;
         ++y;
     }
-    w = z / y;
+    w = double(z) / (double(y) - 1);
     cout << w;
 
     return 0;
